83b767e70d38d4bd22443cca3df9e7f0a8e80548
Manpreet Bhatti <manpreet@bhatti.net>
2024-03-03 01:42:22 -0500

    Finish creating control button

---
+++ src/components/ControlButton/index.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import { IconType } from "react-icons";
+
+type ControlButtonProps = {
+  icon: IconType;
+  link?: string;
+  action?: () => void;
+  [key: string]: any;
+};
+
+const ControlButton: React.FC<ControlButtonProps> = ({
+  icon: Icon,
+  link,
+  action,
+  ...props
+}) => {
+  if (link) {
+    return (
+      <Link
+        {...props}
+        to={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:bg-hover-page active:bg-active-page focus:bg-hover-page mt-2 box-border block aspect-square cursor-pointer rounded-full border-none bg-page text-text transition-colors duration-200 hover:text-primary focus:text-primary"
+      >
+        <Icon {...props} className="flex h-auto w-5" />
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      {...props}
+      className="hover:bg-hover-page active:bg-active-page focus:bg-hover-page mt-2 box-border block aspect-square cursor-pointer rounded-full border-none bg-page text-text transition-colors duration-200 hover:text-primary focus:text-primary"
+    >
+      <Icon className="flex h-auto w-5" />
+    </button>
+  );
+};
+
+export default ControlButton;
