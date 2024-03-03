84968a0d64e8de552606d57323127ccdbb3690e0
Manpreet Bhatti <manpreet@bhatti.net>
2024-03-03 02:48:38 -0500

    Finish functions to return social media icons and strip links

---
+++ src/utils/useMedia.ts
@@ -0,0 +1,34 @@
+import { FiMail, FiGithub } from "react-icons/fi";
+import { FaReact } from "react-icons/fa";
+import { RiLinkedinLine } from "react-icons/ri";
+
+export function stripLink(link: string, type: string) {
+  switch (type) {
+    case "email":
+      return link.replace(
+        "mailto:manpreet@bhatti.net",
+        "manpreet [at] bhatti.net",
+      );
+    case "github":
+      return link.replace("https://github.com/", "");
+    case "linkedin":
+      return link.replace("https://www.linkedin.com/in/", "");
+    default:
+      return link;
+  }
+}
+
+export function getMediaIcon(type: string) {
+  switch (type) {
+    case "email":
+      return FiMail;
+    case "github":
+      return FiGithub;
+    case "linkedin":
+      return RiLinkedinLine;
+    case "react":
+      return FaReact;
+    default:
+      return null;
+  }
+}
