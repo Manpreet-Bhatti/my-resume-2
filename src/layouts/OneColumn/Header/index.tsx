45c40b299ee63862b7079e6f62ae2b8527a86566
Manpreet Bhatti <manpreet@bhatti.net>
2024-03-03 18:08:16 -0500

    Update header logic

--- src/layouts/OneColumn/Header/index.tsx
+++ src/layouts/OneColumn/Header/index.tsx
@@ -23,15 +23,16 @@ const Header: React.FC = () => {
         <span className="text-sm">{blurb}</span>
       </div>
       <ul className="mb-auto flex list-none gap-[0.025in] p-0 text-xs">
-        {Object.keys(data.contentfulHeader).map((key, index) => {
+        {Object.keys(data.contentfulHeader).map((key) => {
           const Icon = getMediaIcon(key);
           return (
-            <li key={index} className="mr-[0.25in]">
+            <li key={key} className="mr-[0.25in]">
               <a
                 href={data.contentfulHeader[key]}
                 rel="noopener noreferrer"
                 target="_blank"
                 className="hover:bg-hover-page active:bg-active-page focus:bg-hover-page flex items-center rounded-lg p-2 font-medium text-alt-text transition-colors duration-200 active:text-primary"
+                aria-label={`Link to ${key}`}
               >
                 {Icon && (
                   <Icon className="mr-[0.05in] text-primary transition-colors duration-200" />
