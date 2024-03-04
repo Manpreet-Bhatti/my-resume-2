c6ad48dd11e98a5bcadf7099c3b791f5e76a2349
Manpreet Bhatti <manpreet@bhatti.net>
2024-03-04 17:44:08 -0500

    Complete gatsby node config

---
+++ gatsby-node.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import type { GatsbyNode } from "gatsby";
+
+export const createPages: GatsbyNode["createPages"] = async ({ actions }) => {
+  const { createPage } = actions;
+  createPage({
+    path: "/",
+    component: path.resolve(__dirname, "./src/layouts/OneColumn/index.tsx"),
+    context: {},
+  });
+};
