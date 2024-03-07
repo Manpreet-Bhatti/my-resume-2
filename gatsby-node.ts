11407509ef0a9731ab19b88ca4eb21a086fb7208
Manpreet Bhatti <manpreet@bhatti.net>
2024-03-06 19:10:21 -0500

    Update

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,11 +1,16 @@
 import * as path from "path";
-import type { GatsbyNode } from "gatsby";
+import { GatsbyNode } from "gatsby";
 
 export const createPages: GatsbyNode["createPages"] = async ({ actions }) => {
   const { createPage } = actions;
-  createPage({
-    path: "/",
-    component: path.resolve("./src/layouts/OneColumn/index.tsx"),
-    context: { withButtons: true },
-  });
+
+  try {
+    createPage({
+      path: "/",
+      component: path.resolve("./src/layouts/OneColumn/index.tsx"),
+      context: { withButtons: true },
+    });
+  } catch (error) {
+    console.error("Error creating pages:", error);
+  }
 };
