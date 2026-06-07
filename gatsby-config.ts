e28971341eac909115f0671bd5d31cf51f2477f9
Manpreet Bhatti <manpreet@bhatti.net>
2026-06-07 16:19:14 -0400

    Remove plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -21,17 +21,6 @@ const config: GatsbyConfig = {
         spaceId: process.env.C_SPACE_ID,
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages",
-      },
-      __key: "pages",
-    },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     "gatsby-plugin-postcss",
     "gatsby-plugin-typescript",
     "gatsby-plugin-netlify",
