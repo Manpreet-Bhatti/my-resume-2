ada197e9662f38c921ecfbd74f9c30d2f9e6cd05
Manpreet Bhatti <manpreet@bhatti.net>
2026-06-23 20:10:16 -0400

    Update

--- src/data/header.ts
+++ src/data/header.ts
@@ -2,5 +2,5 @@ export const header = {
   email: "mailto:manpreet@bhatti.net",
   github: "https://github.com/Manpreet-Bhatti",
   linkedin: "https://linkedin.com/in/manpreet1bhatti",
-  website: "https://manpreetbhatti.com",
+  website: "https://my-site-2-theta.vercel.app",
 };
